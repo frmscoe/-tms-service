11298bfb5abbaa2f0ecb2d477c7966336613cfaf
rtkay123 <rodney.kanjala@sybrin.com>
2023-07-28 13:17:09 +0200

    add[apm]: start example transactions

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,7 @@ import { Pacs008V10Transaction } from './interfaces/iPacs008';
 import { Pain001V11Transaction } from './interfaces/iPain001';
 import { Pain01300109Transaction } from './interfaces/iPain013';
 import { loggerService, server } from './server';
+import apm from 'elastic-apm-node';
 
 const sendToDataPreparation = async (
   data: Pain001V11Transaction | Pain01300109Transaction | Pacs00200112V11Transaction | Pacs008V10Transaction,
@@ -12,12 +13,15 @@ const sendToDataPreparation = async (
 };
 
 export const monitorQuote = async (ctx: Context): Promise<Context> => {
+  const apmTransaction = apm.startTransaction('monitorQuote');
   try {
     const request = ctx.request.body ?? JSON.parse('');
 
     const transaction: Pain001V11Transaction = new Pain001V11Transaction(request);
 
+    const spanToDataPrep = apm.startSpan('send.dataprep', { childOf: apmTransaction?.ids['transaction.id'] });
     await sendToDataPreparation(transaction);
+    spanToDataPrep?.end();
 
     loggerService.log('Request sent to Data Preparation Service');
     ctx.status = 200;
@@ -33,16 +37,21 @@ export const monitorQuote = async (ctx: Context): Promise<Context> => {
       error,
     };
   }
+  apmTransaction?.end();
   return ctx;
 };
 
 export const monitorTransfer = async (ctx: Context): Promise<Context> => {
+  const apmTransaction = apm.startTransaction('monitorTransfer');
   try {
     const request = ctx.request.body ?? JSON.parse('');
 
     const transaction: Pacs008V10Transaction = new Pacs008V10Transaction(request);
 
+    const spanToDataPrep = apm.startSpan('send.dataprep', { childOf: apmTransaction?.ids['transaction.id'] });
     await sendToDataPreparation(transaction);
+    spanToDataPrep?.end();
+
     loggerService.log('Pacs.008 Request sent to Data Preparation Service');
     ctx.status = 200;
     ctx.body = {
@@ -56,16 +65,20 @@ export const monitorTransfer = async (ctx: Context): Promise<Context> => {
       ctx.body = { error: error.stack };
     }
   }
-
+  apmTransaction?.end();
   return ctx;
 };
 export const replyQuote = async (ctx: Context): Promise<Context> => {
+  const apmTransaction = apm.startTransaction('monitorTransfer');
   try {
     const request = ctx.request.body;
 
     const transaction: Pain01300109Transaction = new Pain01300109Transaction(request as Record<string, unknown>);
 
+    const spanToDataPrep = apm.startSpan('send.dataprep', { childOf: apmTransaction?.ids['transaction.id'] });
     await sendToDataPreparation(transaction);
+    spanToDataPrep?.end();
+
     loggerService.log('Request sent to Data Preparation Service');
     ctx.status = 200;
     ctx.body = {
@@ -81,6 +94,7 @@ export const replyQuote = async (ctx: Context): Promise<Context> => {
       error,
     };
   }
+  apmTransaction?.end();
   return ctx;
 };
 
